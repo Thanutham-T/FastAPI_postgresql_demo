c767dc5d1c29a62e411dc915cfbaa2ab3c48196e
Thanutham Tuktian <thanutham.tn@gmail.com>
2025-07-01 11:26:14 +0700

    Refactor run scripts to use Poetry for FastAPI development server

--- Scripts/run-api-dev.bat
+++ Scripts/run-api-dev.bat
@@ -1,9 +1,3 @@
 @echo off
-REM Activate virtual environment if you have one:
-REM Uncomment and edit the line below with your venv path if needed:
-call .\.venv\Scripts\activate
-
-REM Run FastAPI with the development server
-fastapi dev .\src\FastAPI_postgresql_demo\main.py
-
+poetry run fastapi dev .\src\FastAPI_postgresql_demo\main.py
 pause
