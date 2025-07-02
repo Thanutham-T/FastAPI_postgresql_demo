7eec71bb0b0ab7c9eb7a4816e77a5bf2ef584e12
Thanutham Tuktian <thanutham.tn@gmail.com>
2025-07-02 09:57:36 +0700

    Add create_item and delete_item endpoints to main.py

--- FastAPI_postgresql_demo/main.py
+++ FastAPI_postgresql_demo/main.py
@@ -19,6 +19,14 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+@app.post("/items/")
+def create_item(item: Item):
+    return item
+
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+@app.delete("/items/{item_id}")
+async def delete_item(item_id: int):
+    return {"message": f"Item with id {item_id} deleted successfully."}
